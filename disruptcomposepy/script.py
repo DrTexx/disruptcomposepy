76e1cd42b0102d1e99a0e416b696b74c818d4296
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-12 03:46:33 +1000

    call to .convert method on modfiles in bin script

--- disruptcomposepy/script.py
+++ disruptcomposepy/script.py
@@ -5,5 +5,8 @@ def main():
     disco = DisruptCompose()
 
     modfiles = disco.get_modfiles()
+    config = disco.get_config()
+
+    modfiles = [modfile.convert(config) for modfile in modfiles]
 
     print(f"modfiles: {modfiles}")
