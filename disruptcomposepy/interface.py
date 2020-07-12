7f7cf0ee0f539df9d8cc2bd97c7b29953fd409b6
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-12 16:47:38 +1000

    add prepare and write_modfiles methods

--- disruptcomposepy/interface.py
+++ disruptcomposepy/interface.py
@@ -1,13 +1,21 @@
 from . import project, config
-from .paths import project_files_filepath, config_filepath
+from .paths import project_files_filepath, config_filepath, build_path
 
 
 class DisruptComposePy:
     def __init__(self):
         pass
 
+    def prepare(self):
+        project.io.clean_build_dir(build_path=build_path)
+
     def get_modfiles(self):
         return project.io.get_modfiles(project_files_filepath)
+
+    def write_modfiles(self, modfiles, namespace):
+        project.io.write_modfiles(
+            modfiles, build_path=build_path, namespace=namespace
+        )
 
     def get_converters(self):
         return config.io.get_converters(config_filepath)
