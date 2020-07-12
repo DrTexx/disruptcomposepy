0d63dad5ea67bdcc6eceb5969f203a2f57db6178
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-12 17:48:11 +1000

    add ability to write modfiles after mutations

--- disruptcomposepy/project/io.py
+++ disruptcomposepy/project/io.py
@@ -1,7 +1,15 @@
 import json
 import os
+from pathlib import Path
 
 from .modfile import ModFile
+from ..utils import rmdir_recursive, in_directory
+
+FILES_WRITTEN_FILENAME = "FILES_WRITTEN.json"
+
+
+def _fwj(build_path):
+    return Path(build_path).joinpath(FILES_WRITTEN_FILENAME)
 
 
 def get_modfiles(project_files_filepath):
@@ -11,6 +19,7 @@ def get_modfiles(project_files_filepath):
             in the same directory as this binary"""
         )
         raise FileNotFoundError()
+
     with open(project_files_filepath) as f:
         data = json.load(f)
 
@@ -19,3 +28,33 @@ def get_modfiles(project_files_filepath):
         modfiles = [ModFile(file, data_settings) for file in data["files"]]
 
         return modfiles
+
+
+def write_modfiles(modfiles, build_path, namespace):
+    files_written = []
+    for mf in modfiles:
+        if mf.is_mutated:
+            print(f"was mutated: {mf.filepath}")
+            mf.update_filepath(
+                Path(build_path).joinpath(namespace, mf.package_filepath)
+            )
+
+        print(f"writing {mf.filepath}...")
+
+        modfile_dir = Path(mf.filepath).parents[0]
+        modfile_dir.mkdir(parents=True, exist_ok=True)
+
+        with open(mf.filepath, "w") as f:
+            f.write(mf.data)
+
+        files_written.append(str(mf.filepath.absolute()))
+
+        print(mf)
+    files_written_json = _fwj(build_path)
+    with open(files_written_json, "w") as f:
+        json.dump(
+            {"files_written": [w_filepath for w_filepath in files_written]}, f
+        )
+
+    print(files_written)
+
