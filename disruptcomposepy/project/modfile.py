7eaacc165e526429e68fc11e7652b80ea01dd6ea
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-12 17:54:08 +1000

    remaining attributes, add apply mutations method

--- disruptcomposepy/project/modfile.py
+++ disruptcomposepy/project/modfile.py
@@ -37,12 +37,31 @@ def fetch_mutations(entry):
 
 
 class ModFile:
-    def __init__(self, entry):
-        self.filepath = Path(*entry["filepath"])
+    def __init__(self, entry, settings):
+        self.settings = settings
+        self.package_filepath = Path(*entry["filepath"])
+        self.src_filepath = Path(settings["src_root"]).joinpath(
+            self.package_filepath
+        )
+        self.filepath = self.src_filepath
+        self.data = self._read_from_filepath()
         self.filetype = self.filepath.suffix
-        self.methods = resolve_method(entry["method"], self.filetype)
+        self.target = resolve_target(entry["target"], self.filetype)
+        self.target_filepath = self.filepath.with_suffix(self.target)
+        self.mutations = fetch_mutations(entry)
+        self.is_mutated = False
+
+    def _read_from_filepath(self):
+        with open(self.filepath) as f:
+            return f.read()
 
     def __repr__(self):
         out = f"<ModFile targeting '{self.filepath}'>"
 
         return out
+
+    def apply_mutations(self):
+        if self.mutations:
+            self.is_mutated = True
+            for mut in self.mutations:
+                self.data = mut.apply(self)
