ab20196f02fc626e9c092f03c9ef2e46fd4e95df
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-12 17:57:44 +1000

    apply mutations

--- disruptcomposepy/script.py
+++ disruptcomposepy/script.py
@@ -5,8 +5,9 @@ def main():
     disco = DisruptCompose()
 
     modfiles = disco.get_modfiles()
-    c_directory = disco.get_converters()[1]
 
+    [modfile.apply_mutations() for modfile in modfiles]
+    disco.write_modfiles(modfiles, namespace="MUTATED")
 
     c_directory = disco.get_converters()[1]
     [modfile.convert(c_directory) for modfile in modfiles]
